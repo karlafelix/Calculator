83f184be8971b84923eb52e089bc7d7d839f0d05
Karla Félix <karlakaroline_7@hotmail.com>
2019-09-02 00:48:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/app/calculator/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.app.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+        final TextView calculatorScreen = findViewById(R.id.calculatorScreen);
+
+        final Button n0 = findViewById(R.id.zero);
+        final Button n1 = findViewById(R.id.n1);
+        final Button n2 = findViewById(R.id.n2);
+        final Button n3 = findViewById(R.id.n3);
+        final Button n4 = findViewById(R.id.n4);
+        final Button n5 = findViewById(R.id.n5);
+        final Button n6 = findViewById(R.id.n6);
+        final Button n7 = findViewById(R.id.n7);
+        final Button n8 = findViewById(R.id.n8);
+        final Button n9 = findViewById(R.id.n9);
+        final Button dot = findViewById(R.id.dot);
+        final Button equals = findViewById(R.id.equals);
+        final Button addition = findViewById(R.id.addition);
+        final Button subtraction = findViewById(R.id.subtration);
+        final Button multiplication = findViewById(R.id.multiplication);
+        final Button division = findViewById(R.id.division);
+
+        final View.OnClickListener calculatorListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int id = v.getId();
+                switch(id){
+                    case R.id.zero:
+                        break;
+                    case R.id.n1:
+                        break;
+                    case R.id.n2:
+                        break;
+                    case R.id.n3:
+                        break;
+                    case R.id.n4:
+                        break;
+                    case R.id.n5:
+                        break;
+                    case R.id.n6:
+                        break;
+                    case R.id.n7:
+                        break;
+                    case R.id.n8:
+                        break;
+                    case R.id.n9:
+                        break;
+                    case R.id.dot:
+                        break;
+                    case R.id.equals:
+                        break;
+                    case R.id.addition:
+                        break;
+                    case R.id.subtration:
+                        break;
+                    case R.id.multiplication:
+                        break;
+                    case R.id.division:
+                        break;
+
+                }
+            }
+        };
+
+        n0.setOnClickListener(calculatorListener);
+        n1.setOnClickListener(calculatorListener);
+        n2.setOnClickListener(calculatorListener);
+        n3.setOnClickListener(calculatorListener);
+        n4.setOnClickListener(calculatorListener);
+        n5.setOnClickListener(calculatorListener);
+        n6.setOnClickListener(calculatorListener);
+        n7.setOnClickListener(calculatorListener);
+        n8.setOnClickListener(calculatorListener);
+        n9.setOnClickListener(calculatorListener);
+        dot.setOnClickListener(calculatorListener);
+        equals.setOnClickListener(calculatorListener);
+        addition.setOnClickListener(calculatorListener);
+        subtraction.setOnClickListener(calculatorListener);
+        multiplication.setOnClickListener(calculatorListener);
+        division.setOnClickListener(calculatorListener);
+
+
+    }
+}
