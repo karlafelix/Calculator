2db4a119c94704c32dd2caea00dac7c3e6b35157
Karla <karlaf@motorola.com>
2019-09-02 17:38:07 -0400

    Append operation
    Calculadora funcionando a operação adicionar

--- app/src/main/java/com/example/app/calculator/MainActivity.java
+++ app/src/main/java/com/example/app/calculator/MainActivity.java
@@ -8,6 +8,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isOpPressed = false;
+
+    private double firstNumber = 0;
+
+    private int secondNumberIndex = 0;
+
+    private char currentOp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,42 +39,77 @@ public class MainActivity extends AppCompatActivity {
         final Button multiplication = findViewById(R.id.multiplication);
         final Button division = findViewById(R.id.division);
 
+
+
         final View.OnClickListener calculatorListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final int id = v.getId();
                 switch(id){
                     case R.id.zero:
+                        calculatorScreen.append("0");
                         break;
                     case R.id.n1:
+                        calculatorScreen.append("1");
                         break;
                     case R.id.n2:
+                        calculatorScreen.append("2");
                         break;
                     case R.id.n3:
+                        calculatorScreen.append("3");
                         break;
                     case R.id.n4:
+                        calculatorScreen.append("4");
                         break;
                     case R.id.n5:
+                        calculatorScreen.append("5");
                         break;
                     case R.id.n6:
+                        calculatorScreen.append("6");
                         break;
                     case R.id.n7:
+                        calculatorScreen.append("7");
                         break;
                     case R.id.n8:
+                        calculatorScreen.append("8");
                         break;
                     case R.id.n9:
+                        calculatorScreen.append("9");
                         break;
                     case R.id.dot:
+                        calculatorScreen.append(".");
                         break;
                     case R.id.equals:
+                        if(isOpPressed){
+                            if(currentOp == '+'){
+                                String screenContent = calculatorScreen.getText().toString();
+                                String secondNumberString = screenContent.
+                                        substring(secondNumberIndex, screenContent.length());
+                                double secondNumber = Double.parseDouble(secondNumberString);
+                                secondNumber+=firstNumber;
+                                calculatorScreen.setText(String.valueOf(secondNumber));
+                            }
+
+                        }
+
+
                         break;
                     case R.id.addition:
+                        String screenContent = calculatorScreen.getText().toString();
+                        secondNumberIndex = screenContent.length()+1;
+                        firstNumber = Double.parseDouble(calculatorScreen.getText().toString());
+                        calculatorScreen.append("+");
+                        isOpPressed = true;
+                        currentOp = '+';
                         break;
                     case R.id.subtration:
+                        calculatorScreen.append("-");
                         break;
                     case R.id.multiplication:
+                        calculatorScreen.append("*");
                         break;
                     case R.id.division:
+                        calculatorScreen.append("/");
                         break;
 
                 }
@@ -89,6 +132,27 @@ public class MainActivity extends AppCompatActivity {
         subtraction.setOnClickListener(calculatorListener);
         multiplication.setOnClickListener(calculatorListener);
         division.setOnClickListener(calculatorListener);
+
+        final Button delete = findViewById(R.id.del);
+         delete.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View v) {
+                 String displayedElements = calculatorScreen.getText().toString();
+                 int length = displayedElements.length();
+                 if(length>0){
+                    displayedElements = displayedElements.substring(0, length-1);
+                    calculatorScreen.setText(displayedElements);
+                 }
+             }
+         });
+
+         final Button clearEverything = findViewById(R.id.cleareverything);
+         clearEverything.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View v){
+                 calculatorScreen.setText("");
+             }
+         });
 
 
     }
